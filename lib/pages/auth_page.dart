e4d320ea00436b28a272b0974bc0b3a87596b940
Pratyush Pritam <73923732+pratyush-pritam@users.noreply.github.com>
2023-04-14 21:46:26 +0530

    Add files via upload

---
+++ lib/pages/auth_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'home_page.dart';
+import 'login_page.dart';
+
+
+class AuthPage extends StatelessWidget{
+  const AuthPage({super.key});
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot){
+            //user is loggedin
+            if(snapshot.hasData){
+              return HomePage();
+            }
+            else{
+              return LoginPage();
+            }
+
+            
+        },
+        ),
+    );
+  }
+}
